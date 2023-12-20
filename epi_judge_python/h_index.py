84b61c2b6cbe8d02ec0c3e813408c621e4103a85
Ahmed Qureshi <ahmedbq1993@gmail.com>
2023-12-20 12:25:27 -0500

    h_index problem

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -4,8 +4,41 @@ from test_framework import generic_test
 
 
 def h_index(citations: List[int]) -> int:
-    # TODO - you fill in here.
+    
+    ## Brute Force Solution
+    ## Time: O(n^2)
+    ## Space: O(n)
+    # citations.sort()
+    
+    # count = 1
+    # while True:
+    #     local_count = 0
+    #     for cit in citations:
+    #         if cit >= count:
+    #             local_count += 1
+                
+    #     if local_count < count:
+    #         return count - 1
+                
+    #     count += 1
+    
+    
+    ## Optimized Solution
+    ## Time: O(n log n)
+    ## Space: O(n)
+    ### Note: The time complexity is actually the time it takes to sort O(n log n) added onto the 
+    ### amount of time it takes for the loop O(n). But since O(n log n) is slower than O(n), O(n) drops. 
+    citations.sort()
+    length = len(citations)
+    
+    for index, cit in enumerate(citations):
+        # Stop at the first citation where it's >= than the number of remaining papers left
+        if cit >= length - index:
+            return length - index
+    
     return 0
+    
+    
 
 
 if __name__ == '__main__':
